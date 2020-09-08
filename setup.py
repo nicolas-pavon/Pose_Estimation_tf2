6c121add75fe17e9bb283f32517e3ac7bc9e7507
Nicolas Pavon Gomez <npavong@unal.edu.co>
2020-09-07 23:37:00 -0500

    Setup conda-env

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tf_pose',
+      version='0.1',
+      url='https://github.com/nicolas-pavon/Pose_Estimation_tf2',
+      author='Nicolas Pavon Gomez',
+      author_email='npavong@unal.edu.co',
+      packages=['tf_pose'])
